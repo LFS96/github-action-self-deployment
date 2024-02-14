8fc28192ece1e302be5a4cf4e773ef123740055c
Fabian Harmsen <github@lfs96.de>
2024-02-14 14:31:01 +0100

    try selfupload #5

--- update-start.sh
+++ update-start.sh
@@ -12,6 +12,8 @@ docker compose -f docker/docker-compose.yml  pull
 echo "Docker-Images werden gebaut..."
 docker compose -f docker/docker-compose.yml build
 
+echo "Docker-Container werden gestopt..."
+docker compose -f docker/docker-compose.yml down
 # Docker-Container starten
 echo "Docker-Container werden gestartet..."
 docker compose -f docker/docker-compose.yml up -d
